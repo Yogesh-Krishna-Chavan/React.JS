e81b46beebb31d9482827c4826f1a8d59895073a
D3_yogesh_70439 <yogeshchavan113@gmail.com>
2023-09-02 20:04:19 +0530

    NPM Install

--- App.js
+++ App.js
@@ -1,4 +1,27 @@
-const heading = React.createElement("h1", {Id: "heading"}, "Hello world from React..!");
+// const heading = React.createElement("h1", {Id: "heading"}, "Hello world from React..!");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+// console.log(heading);
+
+// const heading = React.createElement("h1", {Id: "heading"}, "Hello World From React");
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+// console.log(heading);
+
+const parent = React.createElement(
+    "div", 
+    {id: "parent"},[
+    React.createElement("div", {id: "child"},[
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+    ]),
+    React.createElement("div", {id: "child"},[
+        React.createElement("h1", {}, "I'm h1 tag"),
+        React.createElement("h2", {}, "I'm h2 tag"),
+        ])
+    ]
+);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
-console.log(heading);
+root.render(parent);
+console.log(parent);
